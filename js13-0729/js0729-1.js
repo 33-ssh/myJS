eba0f4f0bc28c112cae7c60dc15fe7226591e595
33-ssh <mountainxu33@gmail.com>
2024-08-14 16:55:30 +0800

    0814

--- js13-0729/js0729-1.js
+++ js13-0729/js0729-1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // 取得元素節點node
 // 根據id屬性
 // let myjs = document.getElementById('js');
@@ -50,57 +49,4 @@ console.log(myP[0].textContent);
 
 // 取得li子元素中的div底下的p
 let myDivP = document.querySelectorAll('li>div>p');
-=======
-// 取得元素節點node
-// 根據id屬性
-// let myjs = document.getElementById('js');
-// console.log(typeof myjs);
-// console.log(myjs.textContent);
-// console.log(document.querySelector('#js').textContent);
-
-// 根據html標籤
-// let myLi = document.getElementsByTagName('li');
-// console.log(myLi);
-// 取出css
-// console.log(myLi.item(1).textContent);
-// console.log(myLi[1].textContent);
-
-// 顯示所有的li (篩選機制)
-// for(let i=0;i<myLi.length;i++){
-//     console.log(myLi.item(i).textContent);
-//     console.log(myLi[i].textContent);
-// }
-
-
-// console.log('---querySelector---')
-// console.log(document.querySelector('h1'));
-// console.log(document.querySelector('h1').textContent);
-// console.log('---getElementsByTagName---');
-// console.log(document.getElementsByTagName('h1').item(0).textContent);
-// console.log(document.getElementsByTagName('h1')[0].textContent);
-
-// 使用類別名稱
-// let mySubjects = document.getElementsByClassName('subjects');
-// console.log(mySubjects);
-
-// for (let i = 0; i < mySubjects.length; i++) {
-//     console.log(mySubjects.item(i).textContent);
-//     console.log(mySubjects[i].textContent);
-// }
-
-// 顯示指定的內容=>html
-// console.log(mySubjects['css'].textContent);
-
-// 取得id#p1
-console.log(document.getElementById('p1').textContent);
-console.log(document.querySelector('#p1').textContent);
-
-// 取得li子元素中的p
-let myP = document.querySelector('li>p');
-console.log(myP);
-console.log(myP[0].textContent);
-
-// 取得li子元素中的div底下的p
-let myDivP = document.querySelectorAll('li>div>p');
->>>>>>> 7d665d037b9f9a799ee4dbfd19c3fe0f39dd6f05
 console.log(myDivP[0].textContent);
--- js14-0731/js0731-3.js
+++ js14-0731/js0731-3.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // 建立一個存放選取結果的倍數
 let result = "";
 // 取得送出按鈕
@@ -36,42 +35,3 @@ myBtn.addEventListener('click', () => {
     // }
     // console.log('縣市:' + result);
 // });
-=======
-// 建立一個存放選取結果的倍數
-let result = "";
-// 取得送出按鈕
-let myBtn = document.querySelector('#btn');
-// 監聽按鈕是否左鍵一下
-//  add by shan on 2404.07.31
-
-myBtn.addEventListener('click', () => {
-
-    // 取得下拉式方塊
-    let myCity = document.querySelector('#city');
-    // 使用迴圈，逐一檢查是否被選取 =>selected
-        for(let i=0; i<myCity.length;i++){
-            if(myCity[i].selected) {
-                if(myCity[i].value == "") {
-                    result = '尚未選取縣市'
-                } else {
-                    result = myCity[i].value;
-                }
-                break;
-            }
-        }
-        console.log('縣市:'+ result);
-    });
-
-    // for (let i = 0; i < myCity.length; i++) {
-    //     if (myCity[i].selceted) {
-    //         if (myCity[i].value == "") {
-    //             result = '尚未選取縣市'
-    //         } else {
-    //             result = myCity[i].value;
-    //         }
-    //         break;
-    //     }
-    // }
-    // console.log('縣市:' + result);
-// });
->>>>>>> 7d665d037b9f9a799ee4dbfd19c3fe0f39dd6f05
